4f51b402c543df5aa62c0ffa6c120f822f745fad
Pranav Gupta <114329203+xD-kakashi@users.noreply.github.com>
2023-10-31 23:55:50 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//  // Select the container box and the  
+// // mouse placeholder 
+// let main = document.querySelector('.main_box'); 
+// let mouse = document.querySelector('.mouse'); 
+
+// // Add an event listener for detecting 
+// // the movement of the mouse 
+// main.addEventListener('mousemove', 
+//     (e) => { 
+
+//         // Use a circle with a clipPath 
+//         // and the offsetX and offsetY property 
+//         mouse.style.clipPath = 
+// `circle(10em at ${e.offsetX}px ${e.offsetY}px)`; 
+//     });
