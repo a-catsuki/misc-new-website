867b50acf5d03d6f4dda36edc41f2501e7d7e1b6
Pranav Gupta <114329203+xD-kakashi@users.noreply.github.com>
2024-04-17 00:36:39 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -18,15 +18,18 @@ cur_event_details = [['MISC@OWeek','22/02/2024','11:00am','L1 B168', 'static\_MI
                      ['CyberCrêpes','20/03/2024','1:00pm','Carte Crêpes', 'static\-cartecrepes.svg','Bored of diversiTEA? ;) Join us for some chill vibes, networking and free crêpes.','https://use.mazemap.com/#v=1&campusid=200&zlevel=1&center=144.960318,-37.799225&zoom=17.8&sharepoitype=poi&sharepoi=1001584462'],
                      ['FLAGGED 103','21/03/2024','6:30pm','Old Arts (Room 129)', 'static\-flagged103.svg','Fascinated by encryptions and ciphers? Join us to learn about the basics of cryptography with a European guest speaker.','https://use.mazemap.com/#v=1&center=144.960198,-37.798008&zoom=21.1&zlevel=1&campusid=200&sharepoitype=poi&sharepoi=663489'],
                      ['Special General Meeting','28/03/2024','5:30pm','Alan Gilbert (Room 121)', 'static\-sgm.png',"Get ready for an epic evening of delicious pizza and exciting opportunities as you have the chance to join the MISC committee - a perfect way to boost your resume, develop leadership skills, and make an impact in the cybersecurity space. Check our socials for details!",'https://use.mazemap.com/#v=1&campusid=200&zlevel=2&center=144.959195,-37.800231&zoom=21.2&sharepoitype=poi&sharepoi=668402'],
-                     ['FLAGGED 104','18/04/2024','6:30pm','Old Arts (Room 129)', 'static\-flagged104.png','Join us for a 101 on binary exploitation 😏 where we guide you through the basics accompanied by some delicious pizza,','https://use.mazemap.com/#v=1&center=144.960198,-37.798008&zoom=21.1&zlevel=1&campusid=200&sharepoitype=poi&sharepoi=663489']]
+                     ['FLAGGED 104','18/04/2024','6:30pm','Old Arts (Room 129)', 'static\-flagged104.png','Join us for a 101 on binary exploitation (bad pun), where we guide you through the basics, and some ctf challenges accompanied with delicious pizza. ','https://use.mazemap.com/#v=1&center=144.960198,-37.798008&zoom=21.1&zlevel=1&campusid=200&sharepoitype=poi&sharepoi=663489']]
 
 past_event_details = []
 
-ppt_links = ['https://unimelbcloud-my.sharepoint.com/:p:/g/personal/kohlid_student_unimelb_edu_au/EYRywki4gXdNpy1oTYY8j6gBpGmrDLZfA6DcoAPpb6Gdjg?e=9deJKS','https://www.canva.com/design/DAF_q6wO6uA/2O6qKCPoWQplP2uMPFi7UQ/view?utm_content=DAF_q6wO6uA&utm_campaign=designshare&utm_medium=link&utm_source=editor','https://docs.google.com/presentation/d/1PbSWipUnQItzhmt-QsQKHa4ZWMb8gZh1/edit?usp=drive_link&ouid=102696975630975955262&rtpof=true&sd=true']
+ppt_links = ['https://unimelbcloud-my.sharepoint.com/:p:/g/personal/kohlid_student_unimelb_edu_au/EYRywki4gXdNpy1oTYY8j6gBpGmrDLZfA6DcoAPpb6Gdjg?e=9deJKS','https://www.canva.com/design/DAF_q6wO6uA/2O6qKCPoWQplP2uMPFi7UQ/view?utm_content=DAF_q6wO6uA&utm_campaign=designshare&utm_medium=link&utm_source=editor','https://docs.google.com/presentation/d/1PbSWipUnQItzhmt-QsQKHa4ZWMb8gZh1/edit?usp=drive_link&ouid=102696975630975955262&rtpof=true&sd=true','#']
 
 for i in cur_event_details.copy():
-    if "flagged" in i[0].lower():
-        i.append(ppt_links[int(i[0][-1])-1])
+    try:
+        if "flagged" in i[0].lower():
+            i.append(ppt_links[int(i[0][-1])-1])
+    except:
+        pass
     x = i[1].split('/')[:-1]
     print(x)
     if x[-1]<cur_date[-1]:
